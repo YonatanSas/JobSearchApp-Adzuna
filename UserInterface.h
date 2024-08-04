971f3fff7b51d74b551c483496fcc0526dc5b4b8
Yuval Amir <135905839+YuvalAmir1551@users.noreply.github.com>
2024-08-04 15:13:17 +0300

    Update UserInterface.h

--- UserInterface.h
+++ UserInterface.h
@@ -13,9 +13,6 @@
 #include <string>
 // Includes the string class from the C++ Standard Template Library
 
-#include <chrono>
-// Includes the chrono library for time-related functions
-
 class UserInterface {
     // Defines the UserInterface class
 public:
@@ -59,23 +56,7 @@ private:
     bool isSearching;
     // Flag to indicate if a search is in progress
 
-    std::chrono::steady_clock::time_point lastSearchTime;
-    // Time point of the last search operation
-
-    static bool containsIgnoreCase(const std::string& str, const std::string& substr);
-    // Static function to check if a string contains a substring, ignoring case
-
     void toggleFavorite(const Job& job);
     // Function to toggle a job's favorite status
 
-    void loadFavorites();
-    // Function to load favorite jobs
-
-    void saveFavorites();
-    // Function to save favorite jobs
-
-    void updateSearchResults();
-    // Function to update search results
 };
-
-
