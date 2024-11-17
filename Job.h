c9ddaf3df63a5b2567e096e6b5751ffc7547f14c
Yonatan Sasson <146161426+YonatanSas@users.noreply.github.com>
2024-11-17 15:23:16 +0200

    Update Job.h

--- Job.h
+++ Job.h
@@ -2,34 +2,20 @@
 // Prevents multiple inclusions of this header file
 
 #include <string>
-// Includes the string class from the C++ Standard Template Library
 
 struct Job {
     // Defines the Job struct to represent a job listing
     std::string title;
-    // Title of the job
-
     std::string company;
-    // Name of the company offering the job
-
     std::string location;
-    // Location of the job
-
     std::string description;
-    // Description of the job
-
     double salary;
-    // Salary for the job (as a floating-point number)
-
     std::string salaryCurrency;
     // Currency of the salary
-
     std::string contractType;
     // Type of contract for the job
-
     std::string category;
     // Category of the job
-
     std::string url;
     // URL for more details about the job
 
